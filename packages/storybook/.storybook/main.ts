1bd302514caa758d1e2c627119f76d6159f3754e
bohan <bohan-zhang@foxmail.com>
2025-09-21 23:59:02 +0800

    make ci happy

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,11 +1,9 @@
 import path from 'node:path'
 import type { StorybookConfig } from 'storybook-react-rsbuild'
 
-const __dirname = import.meta.dirname
-
 const config: StorybookConfig = {
 	framework: 'storybook-react-rsbuild',
-	stories: [path.resolve(__dirname, '../src/stories/*.stories.tsx')],
+	stories: [path.resolve('src/stories/*.stories.tsx')],
 }
 
 export default config
