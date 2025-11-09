6b6f587b3fa53d896c2d84c55e2306e93984a237
bohan <bohan-zhang@foxmail.com>
2025-11-10 01:19:33 +0800

    html selector orm

--- packages/page/tests/TestLinkPopover/index.test.tsx
+++ packages/page/tests/TestLinkPopover/index.test.tsx
@@ -1,7 +1,20 @@
 import { expect, test } from '@playwright/experimental-ct-react'
+import { HTMLSelectorORM, PageQuery } from '../utils'
 import { TestLinkPopover0 } from './fixture'
 
-test('LinkPopover', async ({ mount }) => {
+test('LinkPopover', async ({ mount, page }) => {
+	const query = PageQuery(page)
+	const selector = HTMLSelectorORM().appendAttribute(
+		'data-slot',
+		'popover-content',
+	)
+
 	const component = await mount(<TestLinkPopover0 />)
-	await expect(component).toContainText('editorLink')
+	await Promise.all([
+		expect(component).toContainText('editorLink'),
+		expect(query.isVisiable(selector)).resolves.toBe(false),
+	])
+
+	await component.click()
+	await expect(query.isVisiable(selector)).resolves.toBe(true)
 })
---
+++ packages/page/tests/utils.ts
@@ -0,0 +1,27 @@
+import type { Page } from 'playwright-core'
+
+export interface HTMLSelectorORM {
+	value(): string
+	appendAttribute(k: string, v: string): HTMLSelectorORM
+}
+
+export function HTMLSelectorORM(): HTMLSelectorORM {
+	let selector = ''
+	return {
+		value(): string {
+			return selector
+		},
+		appendAttribute(k: string, v: string): HTMLSelectorORM {
+			selector += `[${k}="${v}"]`
+			return this
+		},
+	}
+}
+
+export function PageQuery(page: Page) {
+	return {
+		isVisiable(selector: HTMLSelectorORM): Promise<boolean> {
+			return page.isVisible(selector.value())
+		},
+	}
+}
